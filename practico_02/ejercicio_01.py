02df4e1f74693de80789d6dbb62d7510d934503e
giulitruant <giulitruant_26@hotmail.com>
2019-04-08 22:09:47 -0300

    Practico 02. Ejercicios (1,2,3)

--- practico_02/ejercicio_01.py
+++ practico_02/ejercicio_01.py
@@ -4,7 +4,27 @@
 class Rectangulo:
 
     def __init__(self, base, altura):
-        pass
+        self.base = base
+        self.altura = altura
 
     def area(self):
-        pass
+        return "('El area del rectangulo es '{0})".format(self.base * self.altura)
+
+
+base = input('Ingresar base')
+try:
+    bas = int(base)
+    altura = input('Ingresar altura')
+    try:
+        alt = int(altura)
+        resultado = Rectangulo(bas,alt)
+        area = resultado.area()
+        print(area)
+    except ValueError:
+        print('No ingreso correctamente')
+
+except ValueError:
+    print('No ingreso correctamente')
+
+
+
