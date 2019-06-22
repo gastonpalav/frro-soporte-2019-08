50e849dcb60a30cdd0a54998806071f98456859c
giulitruant <giulitruant_26@hotmail.com>
2019-06-22 20:57:08 -0300

    clases creadas (ejercicio 1)

--- practico_05/ejercicio_01.py
+++ practico_05/ejercicio_01.py
@@ -5,14 +5,34 @@
 # - apellido: string (longitud 250)
 
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
+def conexion():
+    engine = create_engine('sqlite:///database.db', echo = True)
+    return engine
+
+def retornarBase():
+    return Base
+
+def sessionUsuario():
+    Base.metadata.bind = conexion()
+    DBSession = sessionmaker()
+    DBSession.bind = conexion()
+    session = DBSession()
+    return  session
 
 class Socio(Base):
     __tablename__ = 'socios'
+    id_socio = Column(Integer, primary_key=True, autoincrement=True, unique=True)
+    dni = Column(Integer, unique=True)
+    nombre = Column(String(250))
+    apellido = Column(String(250))
 
-    # id = Column(...)
-    # dni = Column(...)
-    # nombre = Column(...)
-    # apellido = Column(...)
+def crearModelo():
+    Base.metadata.create_all(conexion())
+    print('Creacion de modelo con exito')
+
+crearModelo()
